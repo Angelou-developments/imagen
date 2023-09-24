4d1cd1be2b6a9e52b7ed480ac0a2993257e32abb
AJ <angelou.developments@gmail.com>
2023-09-24 01:37:43 +0100

    [dbUrls] - update localhost db urls to live render urls

--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -28,7 +28,7 @@ const CreatePost = () => {
     if (form.prompt) {
       try {
         setGeneratingImg(true);
-        const response = await fetch('http://localhost:5173/api/v1/imagen', {
+        const response = await fetch('https://imagen-7co9.onrender.com/api/v1/imagen', {
           method: 'POST',
           headers: {
             'Content-Type': 'application/json',
@@ -56,7 +56,7 @@ const CreatePost = () => {
     if (form.prompt && form.photo) {
       setLoading(true);
       try {
-        const response = await fetch('http://localhost:5173/api/v1/post', {
+        const response = await fetch('https://imagen-7co9.onrender.com/api/v1/post', {
           method: 'POST',
           headers: {
             'Content-Type': 'application/json',
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -26,7 +26,7 @@ const Home = () => {
     setLoading(true);
 
     try {
-      const response = await fetch('http://localhost:5173/api/v1/post', {
+      const response = await fetch('https://imagen-7co9.onrender.com/api/v1/post', {
         method: 'GET',
         headers: {
           'Content-Type': 'application/json',
